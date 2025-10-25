eff8b7fcf804c528f3176826e9aa295d3a64c165
Tahiya07 <tahiyazareen2020@gmail.com>
2025-10-26 03:33:35 +0600

    V 2.9--Tahiya --renaming WatchCraft

--- WatchCraft/asgi.py
+++ WatchCraft/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for watchcraft project.
+ASGI config for WatchCraft project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watchcraft.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WatchCraft.settings')
 
 application = get_asgi_application()
--- WatchCraft/settings.py
+++ WatchCraft/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'watchcraft.urls'
+ROOT_URLCONF = 'WatchCraft.urls'
 
 TEMPLATES = [
     {
@@ -59,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'watchcraft.wsgi.application'
+WSGI_APPLICATION = 'WatchCraft.wsgi.application'
 
 
 # Database
